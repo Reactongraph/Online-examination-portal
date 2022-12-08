d27acd2b140e7bee7852f4cd959efb0f96357ac2
vishnu sharma <vishnu.sharma@ongraph.com>
2022-12-08 11:13:06 +0530

    eslint rules added

--- exam-api/.eslintrc.js
+++ exam-api/.eslintrc.js
@@ -2,18 +2,22 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 'latest'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'standard-with-typescript'
   ],
   root: true,
   env: {
     node: true,
     jest: true,
+    browser: true,
+    es2021: true
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
@@ -21,5 +25,9 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    camelcase: 'off',
+    'global-require': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': ['error', { allow: ['warn'] }],
   },
 };
