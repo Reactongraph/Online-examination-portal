2496b48849ff51056e06795792569235683be253
glalwaniongraph <gaurav.lalwani@ongraphmail.com>
2022-12-13 19:13:00 +0530

    added middleware in all the api

--- exam-api/src/app.module.ts
+++ exam-api/src/app.module.ts
@@ -12,6 +12,10 @@ import { ModuleModule } from './module/module.module';
 import { AuthModule } from './auth/auth.module';
 import { Oraganization } from './organization/organization.middleware';
 import { Participants } from './participants/participants.middlesware';
+import { Modules } from './module/module.middleware';
+import { Levels } from './level/level.middleware';
+import { Questions } from './questions/questions.middleware';
+import { Quiz } from './quiz/quiz.middleware';
 // import { Module, } from '@nestjs/common';
 import { QuestionsController } from './questions/questions.controller';
 import { QuestionsModule } from './questions/questions.module';
@@ -40,6 +44,15 @@ import { QuizService } from './quiz/quiz.service';
     QuestionsController,
     QuizController
   ],
-  providers: [AppService, QuestionsService,PrismaService, QuizService],
+  providers: [AppService, QuestionsService, PrismaService, QuizService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(Oraganization).forRoutes('organization')
+    consumer.apply(Participants).forRoutes('participants')
+    consumer.apply(Modules).forRoutes('module')
+    consumer.apply(Levels).forRoutes('level')
+    consumer.apply(Questions).forRoutes('questions')
+    consumer.apply(Quiz).forRoutes('quiz')
+  }
+}
--- exam-api/src/participants/participants.middlesware.ts
+++ exam-api/src/participants/participants.middlesware.ts
@@ -5,20 +5,16 @@ const prisma = new PrismaClient()
 @Injectable()
 export class Participants implements NestMiddleware {
   async use (req: Request, res: Response, next: NextFunction) {
+    console.log('Request Received')
     prisma.$connect()
-    const user = await prisma.user_auth.findUnique({
-      where: {
-        id: req.headers.id
-      }
-    })
     const Login_token = await prisma.Login.findMany(
       {
         where: {
-          email: user.email
+          token: req.headers.token
         }
       }
     )
-
+    console.log(Login_token)
     if (Login_token.length === 0) {
       res.end('invalid token')
     } else {
