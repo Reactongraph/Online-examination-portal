31cf89877df971158e5055b296d6bf1edb35cb7a
Reactongraph <js.developer@ongraph.com>
2023-01-02 18:19:23 +0530

    middleware integrated

---
+++ exam-app/middleware.js
@@ -0,0 +1,36 @@
+import { NextResponse } from 'next/server'
+
+export async function middleware(req) {
+	const { cookies } = req
+	const url = req.url
+
+	const publicRoutes = ['/login', '/forgotPassword', '/passwordReset']
+
+	const { value } = cookies.getWithOptions('access_token')
+
+	let isAuthenticated = value === undefined ? false : true
+
+	if (publicRoutes.some((v) => url.includes(v))) {
+		if (
+			isAuthenticated &&
+			(url.includes('/login') || url.includes('/signup'))
+		) {
+			return NextResponse.redirect(
+				`${process.env.NEXT_PUBLIC_FRONTEND_LOCAL_URL}/dashboard`
+			)
+		}
+		return NextResponse.next()
+	}
+
+	if (isAuthenticated === false) {
+		return NextResponse.redirect(
+			`${process.env.NEXT_PUBLIC_FRONTEND_LOCAL_URL}/login`
+		)
+	} else {
+		return NextResponse.next()
+	}
+}
+
+export const config = {
+	matcher: ['/dashboard/:path*'],
+}
