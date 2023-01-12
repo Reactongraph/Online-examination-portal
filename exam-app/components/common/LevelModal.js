90b7eccdff80d376de9d6741c6fa0644454a8636
shopify01 <65650555+shopify01@users.noreply.github.com>
2023-01-12 13:21:42 +0530

    creating micro-components

--- exam-app/components/common/LevelModal.js
+++ exam-app/components/common/LevelModal.js
@@ -49,17 +49,50 @@ const LevelModal = (props) => {
 
 	return (
 		<>
-			<LevelModulePopup
-				setStateName={setLevel}
-				stateName={level}
-				checkWithDatabase={checkWithDatabase}
-				handleSubmit={handleSubmit}
-				setModal={setModal}
-				modal={modal}
-				modalName={'LEVEL'}
-				buttonText={buttonText}
-				placeholderText={'eg. Easy , Moderate , etc ...'}
-			/>
+			<PureModal
+				isOpen={modal}
+				width='800px'
+				onClose={() => {
+					setLevel('')
+					setModal(false)
+				}}>
+				<div className='flex-row space-y-3 relative'>
+					<div className='bg-blue-600 p-2 font-bold text-lg text-center text-white -mt-4 -mx-4 mb-5 pb-4'>
+						<p>{buttonText} LEVEL</p>
+					</div>
+
+					<div class='py-6 px-6 lg:px-8'>
+						<form
+							class='w-full max-w-lg'
+							onSubmit={handleSubmit((data) => checkWithDatabase(data))}>
+							<div class='flex flex-wrap -mx-3 mb-6'>
+								<div class='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
+									<Label
+										key='grid-first-name'
+										labelvalue='Enter Level'
+									/>
+									<input
+										class='appearance-none block w-full bg-gray-200 text-gray-700 border  rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
+										id='grid-first-name'
+										type='text'
+										value={level}
+										onChange={(e) => setLevel(e.target.value)}
+										placeholder='eg. Easy , Moderate , etc ...'
+									/>
+								</div>
+							</div>
+
+							<button
+								type='submit'
+								class='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+								{buttonText}
+							</button>
+						</form>
+					</div>
+
+					{/* */}
+				</div>
+			</PureModal>
 
 			<ToastContainer />
 		</>
