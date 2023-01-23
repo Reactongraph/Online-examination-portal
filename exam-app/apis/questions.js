078b8d863e2ea96cc3281e804a51c51b452837de
gaurav <gaurav.lalwani@ongraphmail.com>
2023-01-23 19:56:27 +0530

    added axios instance in partquestion  and quiz

--- exam-app/apis/questions.js
+++ exam-app/apis/questions.js
@@ -1,22 +1,18 @@
-import axios from 'axios'
 import useSWR from 'swr'
-import { SERVER_LINK } from '../helpers/config'
-import { customAxios } from './customAxios'
+import customAxios from './customAxios'
 
-const fetcher = (url, token) =>
-	axios
-		.get(url, {
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json;charset=UTF-8',
-				Authorization: token,
-			},
-		})
-		.then((res) => res.data)
+const fetcher = async (url, token) => {
+	try {
+		const response = await customAxios.get(url)
+		return response.data
+	} catch (error) {
+		return error.response.data
+	}
+}
 
 export function GetQuestionData(token) {
 	const { data, mutate, error, isLoading } = useSWR(
-		[`${SERVER_LINK}/questions/find`, token],
+		[`/questions/find`, token],
 		([url, token]) => fetcher(url, token)
 	)
 	return {
@@ -27,29 +23,18 @@ export function GetQuestionData(token) {
 	}
 }
 export async function GetQuestionDataWithId(token, id) {
-	return await axios.get(`${SERVER_LINK}/questions/find/${id}`, {
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'applications/json;charset=UTF-8',
-			Authorization: token,
-		},
-	})
+	return await customAxios.get(`/questions/find/${id}`)
 }
 export async function DeleteQuestion(id, token) {
-	customAxios.defaults.headers.common.Authorization = token
-	return await customAxios.delete(`${SERVER_LINK}/questions/${id}`)
+	return await customAxios.delete(`/questions/${id}`)
 }
 
 export async function Addquestion(data, token) {
-	customAxios.defaults.headers.common.Authorization = token
-	return await customAxios.post(`${SERVER_LINK}/questions/create`, data)
+	return await customAxios.post(`/questions/create`, data)
 }
 export async function UploadCsvQuestion(data, token) {
-	customAxios.defaults.headers.common.Authorization = token
-	return await customAxios.post(`${SERVER_LINK}/questions/uploads`, data)
-	
+	return await customAxios.post(`/questions/uploads`, data)
 }
 export async function EditQuestion(data, id, token) {
-	customAxios.defaults.headers.common.Authorization = token
-	return await customAxios.patch(`${SERVER_LINK}/questions/${id}`, data)
+	return await customAxios.patch(`/questions/${id}`, data)
 }
--- exam-app/apis/quizzes.js
+++ exam-app/apis/quizzes.js
@@ -1,22 +1,21 @@
 import axios from 'axios'
 import useSWR from 'swr'
 import { SERVER_LINK } from '../helpers/config'
-import { customAxios } from './customAxios'
+import customAxios  from './customAxios'
 
-const fetcher = (url, token) =>
-	axios
-		.get(url, {
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json;charset=UTF-8',
-				Authorization: token,
-			},
-		})
-		.then((res) => res.data)
+const fetcher = async (url, token) =>
+{
+	try {
+		const response = await customAxios.get(url)
+		return response.data
+	} catch (error) {
+		return error.response.data
+	}
+}
 
 export function GetQuizData(token) {
 	const { data, error, isLoading, mutate } = useSWR(
-		[`${SERVER_LINK}/quiz/find`, token],
+		[`/quiz/find`, token],
 		([url, token]) => fetcher(url, token)
 	)
 	return {
@@ -28,24 +27,15 @@ export function GetQuizData(token) {
 }
 
 export async function GetQuizDataWithId(token, id) {
-	return await axios.get(`${SERVER_LINK}/quiz/find/${id}`, {
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'applications/json;charset=UTF-8',
-			Authorization: token,
-		},
-	})
+	return await customAxios.get(`/quiz/find/${id}`)
 }
 export async function DeleteQuiz(id, token) {
-	customAxios.defaults.headers.common.Authorization = token
-	return await customAxios.delete(`${SERVER_LINK}/quiz/${id}`)
+	return await customAxios.delete(`/quiz/${id}`)
 }
 
 export async function AddQuiz(data, token) {
-	customAxios.defaults.headers.common.Authorization = token
-	return await customAxios.post(`${SERVER_LINK}/quiz/create`, data)
+	return await customAxios.post(`/quiz/create`, data)
 }
 export async function EditQuiz(data, id, token) {
-	customAxios.defaults.headers.common.Authorization = token
-	return await customAxios.patch(`${SERVER_LINK}/quiz/${id}`, data)
+	return await customAxios.patch(`/quiz/${id}`, data)
 }
