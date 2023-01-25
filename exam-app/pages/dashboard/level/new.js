f19320ce96721f154a3476778d7d203e7effb13d
harshOGT <harsh.agarwal@ongraphmail.com>
2023-01-25 17:56:39 +0530

    ✨ stating routing for level modals to new page

---
+++ exam-app/pages/dashboard/level/new.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+import Layout from '../../../components/layout/Layout'
+
+export default function AddLevels() {
+	return (
+		<>
+			<Layout title='Add New level'>
+				<h1>Start</h1>
+			</Layout>
+		</>
+	)
+}
