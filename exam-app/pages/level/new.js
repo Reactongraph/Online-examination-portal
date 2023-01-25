637a35ab0099e32bfb536f892f88820fb1140b44
harshOGT <harsh.agarwal@ongraphmail.com>
2023-01-25 18:59:25 +0530

    🌱  added the level module to new route

--- exam-app/pages/level/new.js
+++ exam-app/pages/level/new.js
@@ -1,12 +1,12 @@
 import * as React from 'react'
-
+import AddLevelComponent from '../../../components/level/addLevel'
 import Layout from '../../../components/layout/Layout'
 
 export default function AddLevels() {
 	return (
 		<>
 			<Layout title='Add New level'>
-				<h1>Start</h1>
+				<AddLevelComponent />
 			</Layout>
 		</>
 	)
