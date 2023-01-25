6491c461d31f43c9e9550f519fb3cae85a9bd3ba
harshOGT <harsh.agarwal@ongraphmail.com>
2023-01-25 17:47:18 +0530

    🌱 make a edit request for the quiz with id

--- exam-app/components/quiz/QuizTable.js
+++ exam-app/components/quiz/QuizTable.js
@@ -1,12 +1,9 @@
-import moment from 'moment'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import React from 'react'
 import 'react-pure-modal/dist/react-pure-modal.min.css'
 import { ToastContainer, toast } from 'react-toastify'
 import { injectStyle } from 'react-toastify/dist/inject-style'
 import { DeleteQuiz, EditQuiz } from '../../apis/quizzes'
-import QuizPopUp from '../common/PopUpModals/quizPopUp/QuizPopup'
 import Table from '../common/Table'
 import QuizDataArray from './QuizDataArray'
 import { QuizColumns } from './quizColumn'
@@ -16,31 +13,8 @@ if (typeof window !== 'undefined') {
 	injectStyle()
 }
 
-const QuizTable = ({ data: quiz_data, module_data, level_data }) => {
+const QuizTable = ({ data: quiz_data }) => {
 	const router = useRouter()
-	const [modal, setModal] = useState(false)
-	const [quizId, setQuizId] = useState('')
-
-	const [buttonText, setButtonText] = useState('Add')
-	const [name, setName] = useState('')
-	const levelData = level_data
-	const moduleData = module_data
-
-	const [selectedLevelId, setSelectedLevelId] = useState('')
-	const [description, setDescription] = useState('')
-	const [selectedBufferDate, setSelectedBufferDate] = useState(null)
-	const [selectedStartDate, setSelectedStartDate] = useState(null)
-	const [selectedEndDate, setSelectedEndDate] = useState(null)
-	const [optionModuleSelected, setOptionModuleSelected] = useState()
-	const [selectedModules, setSelectedModules] = useState()
-
-	const { handleSubmit } = useForm()
-
-	const handleLevelTypeSelect = (event) => {
-		let levelId = event.target.value
-		setSelectedLevelId(levelId)
-	}
-
 	const handleRemoveClick = (quiz_id) => {
 		DeleteQuiz(quiz_id)
 			.then(() => {
@@ -51,16 +25,6 @@ const QuizTable = ({ data: quiz_data, module_data, level_data }) => {
 			.catch(() => {
 				toast.error('invalid request')
 			})
-	}
-
-	const handleModuleTypeSelect = (event) => {
-		let moduleSelectedArray = []
-		setOptionModuleSelected(event)
-		event.map((oneModule) => {
-			moduleSelectedArray.push(`${oneModule.id}`)
-		})
-
-		setSelectedModules(moduleSelectedArray)
 	}
 
 	const handleBoxClick = async (quiz_id, quiz_status) => {
@@ -77,53 +41,8 @@ const QuizTable = ({ data: quiz_data, module_data, level_data }) => {
 			})
 	}
 
-	const handleEditClick = async (quiz_id, oneRowData) => {
-		setButtonText('Update')
-
-		setQuizId(quiz_id)
-		setModal(true)
-		let seletedModuleDataArray = []
-
-		// first find the user with the id
-
-		let singleQuizData = oneRowData
-		setName(singleQuizData.quiz_name)
-		setSelectedLevelId(singleQuizData.level_id)
-		setDescription(singleQuizData.description)
-		let bufferDate = moment(singleQuizData.buffer_time).toDate()
-		let startDate = moment(singleQuizData.start_date).toDate()
-		let endDate = moment(singleQuizData.end_date).toDate()
-		moduleData?.map((oneModule) => {
-			singleQuizData.module_id.map((oneID) => {
-				if (oneID.$oid == oneModule.id) {
-					seletedModuleDataArray.push(oneModule)
-				}
-			})
-		})
-		setOptionModuleSelected(seletedModuleDataArray)
-		setSelectedBufferDate(bufferDate)
-		setSelectedEndDate(endDate)
-		setSelectedStartDate(startDate)
-	}
-
-	const checkWithDatabase = async (data) => {
-		data.quiz_name = name
-		data.start_date = selectedStartDate
-		data.end_date = selectedEndDate
-		data.buffer_time = selectedBufferDate
-		data.level_id = selectedLevelId
-		data.description = description
-		data.module_id = selectedModules
-		EditQuiz(data, quizId)
-			.then(() => {
-				setModal(!modal)
-
-				router.replace(router.asPath)
-				toast.success('Quiz updated!')
-			})
-			.catch(() => {
-				toast.error('invalid request')
-			})
+	const handleEditClick = async (quiz_id) => {
+		router.push(`${router.asPath}/edit/${quiz_id}`)
 	}
 
 	// data by using which table data is creating using api call
