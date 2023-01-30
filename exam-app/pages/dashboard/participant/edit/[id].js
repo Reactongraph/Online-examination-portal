dad16a36e3e8a1867313352e4c6e8b6bc9587cd6
gaurav <gaurav.lalwani@ongraphmail.com>
2023-01-30 13:10:25 +0530

    made changes as per comments in pr

--- exam-app/pages/dashboard/participant/edit/[id].js
+++ exam-app/pages/dashboard/participant/edit/[id].js
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import Layout from '../../../../components/layout/Layout'
 import CreateParticipant from '../../../../components/participant/addParticipant'
-export default function EditPage() {
+export default function EditParticipantPage() {
 	return (
 		<>
-			<Layout>
-				<CreateParticipant isViewOnly={false} />
+			<Layout title='Edit Participant'>
+				<CreateParticipant />
 			</Layout>
 		</>
 	)
