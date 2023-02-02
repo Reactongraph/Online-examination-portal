a6b3fe79707e60ee819e6f7ac7f92ba7aa1d9b4d
harshOGT <harsh.agarwal@ongraphmail.com>
2023-02-02 15:41:26 +0530

    🎨 applied nested context provider in organization and participant

--- exam-app/components/highOrderComponents/ParticipantHoc.js
+++ exam-app/components/highOrderComponents/ParticipantHoc.js
@@ -1,39 +1,18 @@
-import { useSelector } from 'react-redux'
-import { GetOrganizationData } from '../../apis/organizations'
-import {
-	GetParticipantData,
-	GetParticipantDataWithOrgId,
-} from '../../apis/participants'
-import { ParticipantContext } from '../context/context'
 import Layout from '../layout/Layout'
+import { OrganizationContextProvider } from '../context/organization_data_context'
+import { ParticipantContextProvider } from '../context/participant_data_context'
 
 export const ParticipantHoc = (Component) => {
 	return function ParticipantHoc(props) {
-		const user = useSelector((state) => state?.user)
-		const { data: organization_data } = GetOrganizationData()
-		const { data, mutate, error } =
-			user?.role == 'SuperAdminUser'
-				? GetParticipantData()
-				: GetParticipantDataWithOrgId(user.Org_id)
-
-		if (data?.error) {
-			return (
-				<>
-					<Layout title='Participant'>
-						<h1>Loading ...</h1>
-					</Layout>
-				</>
-			)
-		}
-
 		return (
 			<>
-				<ParticipantContext.Provider
-					value={{ participant_data: data, organization_data, mutate }}>
-					<Layout title='Participant'>
-						<Component {...props} />
-					</Layout>
-				</ParticipantContext.Provider>
+				<ParticipantContextProvider>
+					<OrganizationContextProvider>
+						<Layout title='Participant'>
+							<Component {...props} />
+						</Layout>
+					</OrganizationContextProvider>
+				</ParticipantContextProvider>
 			</>
 		)
 	}
