3fea59ee4baa5793b846e132e48b14b3197f7ba2
gaurav <gaurav.lalwani@ongraphmail.com>
2023-02-09 16:08:41 +0530

    initial setup for get initialprops

--- exam-app/pages/organization/[id].js
+++ exam-app/pages/organization/[id].js
@@ -4,11 +4,18 @@ import CreateOrganization from '../../components/organization/add_organization'
 import { OrganizationHoc } from '../../hoc/organization_hoc'
 
 const ViewOrganizationWithContext = OrganizationHoc(CreateOrganization)
-export default function ViewOrganizationPage() {
+export default function ViewOrganizationPage({ userId }) {
 	return (
 		<ViewOrganizationWithContext
 			isViewOnly={true}
 			buttonText={'View'}
+			userId={userId}
 		/>
 	)
+}
+
+ViewOrganizationPage.getInitialProps = async ({ query: { id } }) => {
+	return {
+		userId: id,
+	}
 }
