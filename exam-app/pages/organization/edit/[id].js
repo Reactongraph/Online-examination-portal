03f2d6b79c7635d40117cce75a4957d7090e85e3
gaurav <gaurav.lalwani@ongraphmail.com>
2023-02-09 16:43:20 +0530

    implement getIntitialprops in organization view and edit

--- exam-app/pages/organization/edit/[id].js
+++ exam-app/pages/organization/edit/[id].js
@@ -4,11 +4,17 @@ import { OrganizationHoc } from '../../../hoc/organization_hoc'
 
 const EditOrganizationWithContext = OrganizationHoc(CreateOrganization)
 
-export default function EditPage() {
+export default function EditPage({ userId }) {
 	return (
 		<EditOrganizationWithContext
 			buttonText={'Edit'}
 			editform={true}
+			userId={userId}
 		/>
 	)
+}
+EditPage.getInitialProps = async ({ query: { id } }) => {
+	return {
+		userId: id,
+	}
 }
