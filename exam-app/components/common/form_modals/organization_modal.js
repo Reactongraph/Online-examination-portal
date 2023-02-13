9e70eb9cff572f0e91f3e633803fa853b3dae2ac
gaurav <gaurav.lalwani@ongraphmail.com>
2023-02-13 18:22:31 +0530

    fix bug in organization update

--- exam-app/components/common/form_modals/organization_modal.js
+++ exam-app/components/common/form_modals/organization_modal.js
@@ -63,7 +63,10 @@ function OrganizationModal(props) {
 				</div>
 
 				<div className='flex-auto  items-center p-8 bg-white shadow rounded-lg '>
-					<Form onSubmit={handleSubmit((data) => checkWithDatabase(data))}>
+					<Form
+						onSubmit={handleSubmit((data) =>
+							checkWithDatabase(data, organizationId)
+						)}>
 						<React.Fragment>
 							<div className='flex flex-wrap -mx-3 mb-6'>
 								<div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
