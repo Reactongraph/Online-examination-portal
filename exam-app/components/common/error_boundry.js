779d3abb8ff3058249b1d131b551be5a253a5e83
gaurav <gaurav.lalwani@ongraphmail.com>
2023-02-15 16:22:17 +0530

    fix error boundry issue

--- exam-app/components/common/error_boundry.js
+++ exam-app/components/common/error_boundry.js
@@ -1,37 +1,4 @@
-// import React from 'react'
-
-// class ErrorBoundary extends React.Component {
-// 	constructor(props) {
-// 		super(props)
-
-// 		this.state = { hasError: false }
-// 	}
-
-// 	static getDerivedStateFromError() {
-// 		// Update state  true so it will show the fallback UI
-
-// 		return { hasError: true }
-// 	}
-
-// 	render() {
-// 		if (this.state.hasError) {
-// 			return (
-// 				<main class='h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]'>
-// 					<h1 class='text-2xl font-extrabold text-white tracking-widest'>
-// 						Something Went Wrong! Please Try Again
-// 					</h1>
-// 				</main>
-// 			)
-// 		}
-
-// 		// Return children components if hasError is false
-
-// 		return this.props.children
-// 	}
-// }
-
-// export default ErrorBoundary
-export default function MyErrorFallbackComponent({ error }) {
+export default function ErrorFallbackComponent({ error }) {
 	return (
 		<div>
 			<main class='h-screen w-full flex flex-col justify-center items-center bg-[#1A2238] text-white'>
--- exam-app/pages/_app.js
+++ exam-app/pages/_app.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'
 import store from '../store'
 import { injectStyle } from 'react-toastify/dist/inject-style'
 import { ErrorBoundary } from 'react-error-boundary'
-import MyErrorFallbackComponent from '../components/common/error_boundry'
+import ErrorFallbackComponent from '../components/common/error_boundry'
 
 // CALL IT ONCE IN YOUR APP
 if (typeof window !== 'undefined') {
@@ -14,7 +14,7 @@ if (typeof window !== 'undefined') {
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<ErrorBoundary FallbackComponent={MyErrorFallbackComponent}>
+			<ErrorBoundary FallbackComponent={ErrorFallbackComponent}>
 				<Provider store={store}>
 					<Layout>
 						<Component {...pageProps} />
